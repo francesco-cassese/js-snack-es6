dd5727f7cc54c8bb9a22cd36493ff6d3900a8fc6
Francesco Cassese <francescocassese9@gmail.com>
2026-04-09 15:48:23 +0200

    feat: aggiunta funzione validaListaBiciclette in function.js

--- snack1/function.js
+++ snack1/function.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function validaListaBiciclette(listaBiciclette) {
+
+    if (listaBiciclette === null) {
+        return -1
+    }
+
+    if (listaBiciclette.length === 0) {
+        return 0
+    }
+
+    for (let i = 0; i < listaBiciclette.length; i++) {
+
+        const biciCorrente = listaBiciclette[i]
+
+        if (biciCorrente.nome.trim() === '' || biciCorrente.peso <= 0) {
+            return 1
+        }
+    }
+    return listaBiciclette;
+}
