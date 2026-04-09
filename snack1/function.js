5db222c195cc6ac720de3a587357b3cd7038a0f1
Francesco Cassese <francescocassese9@gmail.com>
2026-04-09 16:58:52 +0200

    feat: aggiunta funzione controlloPesoMinore in function.js

--- snack1/function.js
+++ snack1/function.js
@@ -22,3 +22,20 @@ const validaListaBiciclette = listaBiciclette => {
     }
     return listaBiciclette;                     // Se il ciclo termina senza aver trovato errori, la lista è considerata valida restituisco l'intera lista di biciclette
 }
+
+// --- Funzione del controllo peso ---
+
+const controlloPesoMinore = listaBiciclette => {
+
+    let biciLeggera = listaBiciclette[0];      // Considero il primo elemento della lista quello più leggero
+
+    for (let i = 1; i < listaBiciclette.length; i++) {     //Faccio partire il ciclo che controlla tutti gli altri
+
+        const biciCorrente = listaBiciclette[i];           //Salvo l'elemento corrente in una costante per rendere il codice più leggibile
+
+        if (biciLeggera > biciCorrente.peso) {             //Se ad ogni giro la bici considerata leggera ne trova una con un peso minore 
+            biciLeggera = biciCorrente;                    //Allora quella diventa la biciLeggera
+        }
+    }
+    return biciLeggera;                                    //Alla fine di tutto mi faccio restituire la bici più leggera
+}
