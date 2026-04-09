07f0b912dc5b738f474a5349f83bbefba88bdca4
Francesco Cassese <francescocassese9@gmail.com>
2026-04-09 17:51:53 +0200

    refacor: resa funzione validaCatalogoBiciclette universale

--- snack1/function.js
+++ snack1/function.js
@@ -4,27 +4,27 @@
 // VALIDAZIONE LISTA BICICLETTE
 //===================================================
 
-const validaListaBiciclette = listaBiciclette => {
+const validaListaBiciclette = lista => {
 
-    if (!listaBiciclette) {                     // Controllo se l'argomento passato esiste (non è nullo o undefined)
+    if (!lista) {                     // Controllo se l'argomento passato esiste (non è nullo o undefined)
         return -1                               // Se la lista non è stata definita, restituisco il codice di errore -1
     }
 
-    if (listaBiciclette.length === 0) {         // Controllo se l'array è vuoto verificando la sua lunghezza
+    if (lista.length === 0) {         // Controllo se l'array è vuoto verificando la sua lunghezza
         return 0                                // Se non ci sono elementi all'interno, restituisco il codice 0
     }
 
-    for (let i = 0; i < listaBiciclette.length; i++) {  // Avvio un ciclo for per esaminare ogni elemento della lista
+    for (let i = 0; i < lista.length; i++) {  // Avvio un ciclo for per esaminare ogni elemento della lista
 
-        const biciCorrente = listaBiciclette[i]         // Salvo l'elemento corrente in una costante per rendere il codice più leggibile
+        const elemCorrente = lista[i]         // Salvo l'elemento corrente in una costante per rendere il codice più leggibile
 
-        const pesoConvertito = Number(biciCorrente.peso);   // Converto il peso in numero
+        const numConvertito = Number(elemCorrente.peso);   // Converto il peso in numero
 
-        if (biciCorrente.nome.trim() === '' || isNaN(pesoConvertito) || pesoConvertito <= 0) {      // Eseguo la validazione delle proprietà dell'oggetto corrente
+        if (elemCorrente.nome.trim() === '' || isNaN(numConvertito) || numConvertito <= 0) {      // Eseguo la validazione delle proprietà dell'oggetto corrente
             return 1                                                                                // Se anche una sola bicicletta non è valida, restituisco il codice di errore 1
         }
     }
-    return listaBiciclette;                     // Se il ciclo termina senza aver trovato errori, la lista è considerata valida restituisco l'intera lista di biciclette
+    return lista;                     // Se il ciclo termina senza aver trovato errori, la lista è considerata valida restituisco l'intera lista di biciclette
 }
 
 //===================================================
