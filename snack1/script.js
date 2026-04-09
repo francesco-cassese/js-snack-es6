67b4baf608a2a2eb6ba8be75478bcf4aeafdaf57
Francesco Cassese <francescocassese9@gmail.com>
2026-04-09 14:36:56 +0200

    feat: aggiunto catalogo biciclette in script.js

--- snack1/script.js
+++ snack1/script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+// catalogo.js
+
+const catalogoBiciclette = [
+    {
+        nome: "Specialized S-Works Tarmac SL8",
+        peso: 6.6
+    },
+    {
+        nome: "Trek Emonda SLR 9",
+        peso: 6.6
+    },
+    {
+        nome: "Cannondale SuperSix EVO Lab71",
+        peso: 6.8
+    },
+    {
+        nome: "Canyon Ultimate CFR",
+        peso: 6.3
+    },
+    {
+        nome: "Pinarello Dogma F",
+        peso: 6.9
+    },
+    {
+        nome: "Giant TCR Advanced SL 0",
+        peso: 6.4
+    },
+    {
+        nome: "Wilier Filante SLR",
+        peso: 6.8
+    },
+    {
+        nome: "Bianchi Specialissima RC",
+        peso: 6.6
+    },
+    {
+        nome: "Colnago V4Rs",
+        peso: 6.9
+    },
+    {
+        nome: "Scott Addict RC Ultimate",
+        peso: 6.7
+    }
+];
